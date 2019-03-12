ee919403387024866b61fd0112801755ffea5fcd
Adongre <dongre.avinash@gmail.com>
2019-03-12 12:44:34 +0530

    Adding API for create volume

--- src/org/opendedup/sdfs/mgmt/CreateVolumeWeb.java
+++ src/org/opendedup/sdfs/mgmt/CreateVolumeWeb.java
@@ -1,17 +1,31 @@
 package org.opendedup.sdfs.mgmt;
 
 import org.opendedup.logging.SDFSLogger;
+import org.opendedup.sdfs.VolumeConfigWriter;
+import org.opendedup.util.OSValidator;
 import org.opendedup.util.XMLUtils;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 
 import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 public class CreateVolumeWeb {
-    public Element getResult(String cmd, String file) throws IOException {
+    public Element getResult(String options, String file) throws IOException {
         try {
-            return (Element) this.toXMLDocument(cmd).getDocumentElement()
+            File f = new File(OSValidator.getConfigPath());
+            if (!f.exists())
+                f.mkdirs();
+
+            VolumeConfigWriter wr = new VolumeConfigWriter();
+            List<String> items = Arrays.asList(options.split("\\s*,\\s*"));
+            wr.parseCmdLine(items.toArray(new String[items.size()]));
+            wr.writeConfigFile();
+
+            return (Element) this.toXMLDocument("Success").getDocumentElement()
                     .cloneNode(true);
         } catch (Exception e) {
             SDFSLogger.getLog().error(
@@ -20,6 +34,7 @@ public class CreateVolumeWeb {
                     + e.toString());
         }
     }
+
     private Document toXMLDocument(String messssage) throws ParserConfigurationException {
         Document doc = XMLUtils.getXMLDoc("create-volume");
         Element root = doc.getDocumentElement();
